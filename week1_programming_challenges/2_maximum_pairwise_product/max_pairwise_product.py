241bfb3decbceb50e5a094a84871883c45116d7c
Marcelo Rosa <marcelomapr@gmail.com>
2018-10-25 20:29:34 -0300

    dada

---
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -0,0 +1,22 @@
+# python3
+
+
+def max_pairwise_product(numbers):
+    n = len(numbers)
+
+    numbers.sort()
+
+    max_product = numbers[-1] * numbers[-2]
+    #max_product = 0
+    #for first in range(n):
+    #    for second in range(first + 1, n):
+    #        max_product = max(max_product,
+    #                          numbers[first] * numbers[second])
+
+    return max_product
+
+
+if __name__ == '__main__':
+    input_n = int(input())
+    input_numbers = [int(x) for x in input().split()]
+    print(max_pairwise_product(input_numbers))
